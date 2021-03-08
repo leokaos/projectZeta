479df27a6050f23fec0892d131d99a6acb36e0d3
leo <leokaos@gmail.com>
2021-03-08 11:36:50 +0100

    paineis da dashboard

--- client/src/app/components/dashboard/dash.component.ts
+++ client/src/app/components/dashboard/dash.component.ts
@@ -1,33 +1,38 @@
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Component } from '@angular/core';
+import { AfterViewChecked, AfterViewInit, Component, ComponentFactoryResolver, OnInit, QueryList, ViewChild, ViewChildren, ViewContainerRef } from '@angular/core';
 import { map } from 'rxjs/operators';
+import { PanelCandidatosComponent } from '../panel-candidatos/panel-candidatos.component';
+import { PanelVagasComponent } from '../panel-vagas/panel-vagas.component';
 
 @Component({
   selector: 'app-dash',
   templateUrl: './dash.component.html',
   styleUrls: ['./dash.component.css']
 })
-export class DashComponent {
+export class DashComponent implements AfterViewInit {
 
-  cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
-    map(({ matches }) => {
-      if (matches) {
-        return [
-          { title: 'Card 1', cols: 1, rows: 1 },
-          { title: 'Card 2', cols: 1, rows: 1 },
-          { title: 'Card 3', cols: 1, rows: 1 },
-          { title: 'Card 4', cols: 1, rows: 1 }
-        ];
+  cards = [
+    { title: 'Card 1', cols: 1, rows: 1, class: PanelVagasComponent },
+    { title: 'Card 2', cols: 1, rows: 1, class: PanelVagasComponent },
+    { title: 'Card 3', cols: 1, rows: 1, class: PanelVagasComponent },
+    { title: 'Card 4', cols: 3, rows: 1, class: PanelCandidatosComponent }
+  ];
+
+  @ViewChildren('dynamic', { read: ViewContainerRef })
+  viewContainerRefList: QueryList<ViewContainerRef>;
+
+  constructor(private factoryResolver: ComponentFactoryResolver) {
+  }
+
+  ngAfterViewInit(): void {
+
+    this.viewContainerRefList.map(
+      (vcr: ViewContainerRef, index: number) => {
+        const factory = this.factoryResolver.resolveComponentFactory(this.cards[index].class);
+        vcr.createComponent(factory);
       }
+    );
 
-      return [
-        { title: 'Card 1', cols: 2, rows: 1 },
-        { title: 'Card 2', cols: 1, rows: 1 },
-        { title: 'Card 3', cols: 1, rows: 2 },
-        { title: 'Card 4', cols: 1, rows: 1 }
-      ];
-    })
-  );
+  }
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
 }
